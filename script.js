beb031714d7d4d1281e80bd7dd00f4907e28aecf
4FunkE <4FunkEcodes@gmail.com>
2023-05-01 17:45:11 -0400

    scaffolded javascript using the criteria

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-// Assignment code here
+//prompts for password criteria
+//select which criteria to use- if select else if, if
 
+//prompt the length of password-choose 8-128
+
+//prompt character type-choose lower, upper, numeric, or special character
+
+//answer to each prompt-answer chould be validated and one character type should be selected
+
+//when all prompts are answered-password is generated-either in alert or written on page
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");

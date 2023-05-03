0a9fabe3178014e2871f71c0a11e9ff9968dde6e
4FunkE <4FunkEcodes@gmail.com>
2023-05-02 22:25:00 -0400

    I linked the button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //prompt the length of password-choose 8-128, //prompt character type-choose lower, upper, numeric, or special character
 var userChoice = {
-    lowerCase: "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z",
-    upperCase: "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z",
-    numeric: 1234567890,
+    lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z'],
+    upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    numeric: [1,2,3,4,5,6,7,8,9,0],
     specialCharacter: "~`!@#$%^&*()_-=+{}[]\|/;:'<>,.?",
 };
 
@@ -12,21 +12,20 @@ console.log(userChoice.numeric);
 console.log(userChoice.specialCharacter);
 //prompts for password criteria
 
-
 //answer to each prompt-answer chould be validated and one character type should be selected
-
-//when all prompts are answered-password is generated-either in alert or written on page
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 //when you press the generate button a window prompts 
 generateBtn = function() {
-    var userChoice = window.prompt("How many characters would you like your password to have?") 
-    if(!userChoice) { //if thr user choice (variable) NOT (!) cancled then end (return)
+    var onClick = window.prompt("How many characters would you like your password to have? Choose 8-128"); 
+    if(!onClick) { //if thr user choice (variable) NOT (!) cancled then end (return)
         return;
     }
 }
+
+//when all prompts are answered-password is generated-either in alert or written on page
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

d4ef38c566dbacbdac171da430451f7acfcd48ec
4FunkE <4FunkEcodes@gmail.com>
2023-06-12 12:17:38 -0400

    fixe window prompt password length parameter

--- script.js
+++ script.js
@@ -5,20 +5,17 @@ var userChoiceOptions = {
     numeric: [1,2,3,4,5,6,7,8,9,0],
     specialCharacter: ['!', '@' , '#', '$', '%', '^', '&', '*', '?'],
 };
-
-//named variables
-var chosenLower;
-var chosenUpper;
-var chosenNumeric;
-var chosenSpecial;
-
-var userChoice;
-
 console.log(userChoiceOptions.lowerCase);
 console.log(userChoiceOptions.upperCase);
 console.log(userChoiceOptions.numeric);
 console.log(userChoiceOptions.specialCharacter);
 
+//named variables
+// var chosenLower;
+// var chosenUpper;
+// var chosenNumeric;
+// var chosenSpecial;
+// var userChoice;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -30,16 +27,17 @@ var generateBtn = function() {
     var onClick = window.prompt("How many characters would you like your password to have? Choose 8-128"); 
     
     //must use 8-128
-    if (onClick < 8 || onClick > 128) {
-        onClick = parseInt(alert("Invalid: Please choose a number from 8-128."));
-    } else {
-    //all the other criteria choices
-        var userChoiceSpecial = confirm("Would you like to include special characters in your password?");
-        var userChoiceLower = confirm("Would you like to include lowercase letters in your password?");
-        var userChoiceUpper = confirm("Would you like to include uppercase letters in your password?");
-    } 
+    if (onClick < 8 || onClick > 128 || isNaN(onClick)) {
+        alert("Invalid: Please choose a number from 8-128.");
+        return; 
+    }
+    
+    
+        // //all the other criteria choices
+        // var userChoiceSpecial = confirm("Would you like to include special characters in your password?");
+        // var userChoiceLower = confirm("Would you like to include lowercase letters in your password?");
+        // var userChoiceUpper = confirm("Would you like to include uppercase letters in your password?");
 };
-
 
 
 //START HERE!
@@ -49,7 +47,7 @@ if (chosenLower && chosenUpper && chosenNumeric && chosenSpecial) {
 }
 
 //generate a random password function for computer
-for (var i = 0; i < enter; i++) {
+for (var i = 0; i < userChoiceOptions.length; i++) {
     var userChoiceList = choices[Math.floor(Math.random() * choices.length)];
     password.push(pickChoices);
 }
@@ -57,7 +55,8 @@ for (var i = 0; i < enter; i++) {
 
 // Write password to the #password input
 function writePassword(password) {
-  var password = generatePassword.join("");
+    var password = "";
+  //var password = generatePassword.join("");
   var passwordText = document.querySelector("#password");
     //where is the data?
     //userInput(password);
